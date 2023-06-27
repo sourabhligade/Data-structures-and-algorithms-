28c75ccd615f5a3facdacef2b54e5397786568a6
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-06-27 09:07:11 +0530

    Create C-1.14.py

---
+++ C-1.14.py
@@ -0,0 +1,17 @@
+C-1.14 Write a short Python function that takes a sequence of integer values and determines 
+if there is a distinct pair of numbers in the sequence whose product is odd.
+
+
+
+def hasDistinctOddProduct(sequence):
+    odd_numbers=set()
+    for num in sequence:
+      if num %2!=0:
+        if any((num * other_num)%2!=0 for other_num in sequence):
+            return True
+        odd_numbers.add(num)
+    return False
+
+sequene=[1, 2, 3, 4, 5]
+print(hasDistinctOddProduct(sequence))
+    
