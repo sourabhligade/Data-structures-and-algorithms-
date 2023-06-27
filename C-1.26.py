8549b61818f18b7c4b8a2b681c3a88f1ec82f0c1
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-06-27 14:28:46 +0530

    Create C-1.26.py

---
+++ C-1.26.py
@@ -0,0 +1,25 @@
+Write a short program that takes as input three integers, a, b, and c, from the console and determines if they can be used
+in a correct arithmetic formula (in the given order), like “a+b = c,” “a = b−c,” or “a∗b = c.”
+
+
+
+
+
+
+
+
+def intergerss(a,b,c):
+    if a+b==c:
+        return f"{a}+{b}=={c}"
+    elif a==b-c:
+        return f"{a}=={b}-{c}"
+    elif a*b==c:
+        return f"{a}*{b}==c"
+    else:
+        return "no artihmentic sequence found"
+    
+
+a=int(input('a'))
+b=int(input('b'))
+c=int(input('c'))
+print(intergerss(a,b,c))
