5009280d2f5b2aa9ef9a55224b4fa67fbe466167
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-06-27 14:44:28 +0530

    Create C-1.27.py

---
+++ C-1.27.py
@@ -0,0 +1,21 @@
+
+In Section 1.8, we provided three different implementations of a generator that computes factors of a given integer. The third of those implementa- tions, from page 41, was the most efficient, but we noted that it did not yield the factors in increasing order. 
+Modify the generator so that it reports factors in increasing order, while maintaining its general performance ad- vantages.
+
+
+
+def factors(n):
+    k=1
+    while k*k<n: 
+        if n % k == 0:
+            yield k
+            yield n // k 
+        k += 1
+    if k*k == n:
+        yield k
+
+n=100
+p=list(factors(n))
+p.sort()
+for factor in p:
+    print(factor)
