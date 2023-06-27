7c581e05974986a5761609e4a9d87ab18c1d2a61
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-06-27 12:13:51 +0530

    Create C-1.21.py

---
+++ C-1.21.py
@@ -0,0 +1,15 @@
+#Write a Python program that repeatedly reads lines from standard input until an EOFError is raised, and then outputs
+those lines in reverse order (a user can indicate end of input by typing ctrl-D).
+
+
+lines=[]
+
+
+try:
+  while True:
+    line=input()
+    lines.append(line)
+except:
+  EOFerror
+  for line in reversed(lines):
+    print(line)
