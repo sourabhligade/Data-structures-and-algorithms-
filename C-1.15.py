a60bdae98038894fc75c39d16930f47f1a4fb706
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-08-24 22:36:09 +0530

    Update C-1.15.py

--- C-1.15.py
+++ C-1.15.py
@@ -1,8 +1,6 @@
 #Write a Python function that takes a sequence of numbers and 
 #determines if all the numbers are different from each other (that is, they are distinct).
 
-
-
 def hasDistinctnum(sequence):
     for num in sequence:
         for other_num in sequence:
