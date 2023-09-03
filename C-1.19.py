e6cdb2dc27f2360862ef7e9a0dcaa9c1a118a35d
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2023-09-03 23:50:46 +0530

    Update C-1.19.py

--- C-1.19.py
+++ C-1.19.py
@@ -6,3 +6,4 @@ thelist[ a , b , c ,..., z ],butwithouthavingtotypeall26such characters literall
 import string
 alphabet_sequence=[char for char in string.ascii_lowecase]
 print(alphabet_sequence)
+
