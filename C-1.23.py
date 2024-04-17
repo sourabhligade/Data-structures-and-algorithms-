75d40ea4dbe12a703fafa2c8be612acaf68abdb9
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-04-17 19:49:19 +0530

    Update C-1.23.py

--- C-1.23.py
+++ C-1.23.py
@@ -1,4 +1,4 @@
-Give an example of a Python code fragment that attempts to
+ .Give an example of a Python code fragment that attempts to
 write an ele- ment to a list based on an index that may be out of bounds. If that index is out of bounds, the program should catch the exception that results, and print the following error message:
 “Don’t try buffer overflow attacks in Python!”
 
