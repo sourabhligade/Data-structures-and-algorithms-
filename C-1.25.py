628252c9b04751ffee6f8afbf889d4a03fba97d0
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-04-21 21:42:25 +0530

    Update C-1.25.py

--- C-1.25.py
+++ C-1.25.py
@@ -1,4 +1,4 @@
-
+#hasgvhasc
 WriteashortPythonfunctionthattakesastrings,representingasentence,
 and returns a copy of the string with all punctuation removed. 
 For exam- ple, if given the string "Let s try, Mike.", this function would return "Lets try Mike".
