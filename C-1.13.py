d5126a18290b8e819298dc9984bd5045eafaeb52
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-05-07 23:05:02 +0530

    Update C-1.13.py

--- C-1.13.py
+++ C-1.13.py
@@ -2,7 +2,6 @@
 C-1.13
 
 #Write a pseudo-code description of a function that reverses a list of n integers, so that the numbers are listed in the 
-#opposite order than they were before, and compare this method to an equivalent Python function for doing the same thing
 
 
 def reVerse(n):
