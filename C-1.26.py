08934046299a16176067eff6b4222d9a885f1b8e
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-05-25 23:14:00 +0530

    Update C-1.26.py

--- C-1.26.py
+++ C-1.26.py
@@ -1,4 +1,4 @@
-Write a short program that takes as input three integers, a, b, and c, from the console and determines if they can be used
+Write a short program that takes as input three integers, a, b, and c, from the console and deterlmines if they can be used
 in a correct arithmetic formula (in the given order), like “a+b = c,” “a = b−c,” or “a∗b = c.”.
 
 
