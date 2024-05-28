0c789bbd5a8cdd1faf825e2e97065c7a04c4ba65
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-05-28 22:04:07 +0530

    Update C-1.14.py

--- C-1.14.py
+++ C-1.14.py
@@ -1,5 +1,5 @@
 
-C-1.14 Write a short Python function that takes a sequence of integer values and determines 
+C-1.14 Write a short Python function that takes a sequence of integer values and dtermines 
 if there is a distinct pair of numbers in the sequence whose product is odd
 
 
