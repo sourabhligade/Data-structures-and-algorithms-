a0a358977e38d261f206942b8c68ff43bf869490
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-06-30 22:30:25 +0530

    Update C-1.15.py

--- C-1.15.py
+++ C-1.15.py
@@ -22,19 +22,6 @@ Write a Python function that takes a sequence of numbers and
 
 
 
-def hasDistinctnum(sequence):
-    for num in sequence:
-        for other_num in sequence:
-            if (num!=other_num):
-                return True
-    else:
-        return False
-
-
-
-
-sequence=[1,2,3,4]
-print(hasDistinctnum(sequence))
             
 
        
