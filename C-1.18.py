20ce43aaf89b7110af9d36ce845331350b5b02c1
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2024-07-12 23:38:43 +0530

    Update C-1.18.py

--- C-1.18.py
+++ C-1.18.py
@@ -1,5 +1,4 @@
-Simple levl
-Demonstrate how to use Python’s list comprehension syntax to produce
+ Demonstrate how to use Python’s list comprehension syntax to produce
 the list [0, 2, 6, 12, 20, 30, 42, 56, 72, 90].
 
 results=[i*(i+1) for i in range (10)]
