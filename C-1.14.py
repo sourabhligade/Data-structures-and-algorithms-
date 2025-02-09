f2821329eb74a6c0dc58755bae1188b08221ff9f
Sourabh Ligade <65074119+sourabhligade@users.noreply.github.com>
2025-02-09 23:52:58 +0530

    Update C-1.14.py

--- C-1.14.py
+++ C-1.14.py
@@ -1,2 +1,3 @@
 
 
+ finished scraping Error scraping jobs for Amazon: 'float' object has no attribute 'strip' 2025-02-09 23:49:39,564 - INFO - JobSpy:Glassdoor - finished scraping Error scraping jobs for BNY Mellon: 'float' object has no attribute 'strip' 2025-02-09 23:49:39,709 - INFO - JobSpy:Glassdoor - finished scraping Error scraping jobs for Swiggy: 'float' object has no attribute 'strip'
